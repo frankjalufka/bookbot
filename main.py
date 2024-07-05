43f10f92100ab713c2ac268ca2c0f6361a802dc4
Frank Jalufka <fljalufka@gmail.com>
2024-07-05 12:57:32 -0500

    added code to count words

---
+++ main.py
@@ -0,0 +1,18 @@
+
+
+def main():
+    book_path = "/Users/fljalufka/workspace/github.com/frankjalufka/bookbot/books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    print(f"{num_words} words found in the document")
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+if __name__ =='__main__':
+    main()
