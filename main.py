9e683a1217cc2067193eec2d159d0caf748b80e2
Frank Jalufka <fljalufka@gmail.com>
2024-07-05 13:13:44 -0500

    Added report feature

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
-
-
 def main():
-    book_path = "/Users/fljalufka/workspace/github.com/frankjalufka/bookbot/books/frankenstein.txt"
+    book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+
+    print(f"--- Begin report of {book_path} ---")
     print(f"{num_words} words found in the document")
+    print()
+
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"The'{item['char']}' character was found {item['num']} times")
+
+    print("--- End report ---")
+
 
 def get_num_words(text):
     words = text.split()
@@ -14,5 +25,25 @@ def get_book_text(path):
     with open(path) as f:
         return f.read()
 
-if __name__ =='__main__':
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
+
+def sort_on(d):
+    return d["num"]
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
+
+if __name__ == '__main__':
     main()
